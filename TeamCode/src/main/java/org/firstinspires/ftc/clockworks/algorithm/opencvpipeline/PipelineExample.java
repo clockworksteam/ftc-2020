afa3abc5834e3dc3af75618a2c1b9af516a0d2bc
tudorcioc3510 <tudorcioc@gmail.com>
2019-12-21 23:02:56 +0200

    New package for OpenCV pipeline

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/clockworks/algorithm/opencvpipeline/PipelineExample.java
@@ -0,0 +1,68 @@
+package org.firstinspires.ftc.clockworks.algorithm.opencvpipeline;
+
+import org.opencv.core.Mat;
+import org.opencv.core.Point;
+import org.opencv.core.Scalar;
+import org.opencv.imgproc.Imgproc;
+import org.openftc.easyopencv.OpenCvPipeline;
+
+/*
+ * An example image processing pipeline to be run upon receipt of each frame from the camera.
+ * Note that the processFrame() method is called serially from the frame worker thread -
+ * that is, a new camera frame will not come in while you're still processing a previous one.
+ * In other words, the processFrame() method will never be called multiple times simultaneously.
+ *
+ * However, the rendering of your processed image to the viewport is done in parallel to the
+ * frame worker thread. That is, the amount of time it takes to render the image to the
+ * viewport does NOT impact the amount of frames per second that your pipeline can process.
+ *
+ * IMPORTANT NOTE: this pipeline is NOT invoked on your OpMode thread. It is invoked on the
+ * frame worker thread. This should not be a problem in the vast majority of cases. However,
+ * if you're doing something weird where you do need it synchronized with your OpMode thread,
+ * then you will need to account for that accordingly.
+ */
+
+
+public class PipelineExample extends OpenCvPipeline {
+    /*
+     * NOTE: if you wish to use additional Mat objects in your processing pipeline, it is
+     * highly recommended to declare them here as instance variables and re-use them for
+     * each invocation of processFrame(), rather than declaring them as new local variables
+     * each time through processFrame(). This removes the danger of causing a memory leak
+     * by forgetting to call mat.release(), and it also reduces memory pressure by not
+     * constantly allocating and freeing large chunks of memory.
+     */
+
+    @Override
+    public Mat processFrame(Mat input)
+    {
+        /*
+         * IMPORTANT NOTE: the input Mat that is passed in as a parameter to this method
+         * will only dereference to the same image for the duration of this particular
+         * invocation of this method. That is, if for some reason you'd like to save a copy
+         * of this particular frame for later use, you will need to either clone it or copy
+         * it to another Mat.
+         */
+
+        /*
+         * Draw a simple box around the middle 1/2 of the entire frame
+         */
+        Imgproc.rectangle(
+                input,
+                new Point(
+                        input.cols()/4,
+                        input.rows()/4),
+                new Point(
+                        input.cols()*(3f/4f),
+                        input.rows()*(3f/4f)),
+                new Scalar(0, 255, 0), 4);
+
+        /**
+         * NOTE: to see how to get data from your pipeline to your OpMode as well as how
+         * to change which stage of the pipeline is rendered to the viewport when it is
+         * tapped, please see {@link PipelineStageSwitchingExample}
+         */
+
+        return input;
+    }
+}
--- TeamCode/src/main/java/org/firstinspires/ftc/clockworks/teleop/test/OpenCVWebcamExample.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/clockworks/teleop/test/OpenCVWebcamExample.java
@@ -11,7 +11,6 @@ import org.opencv.imgproc.Imgproc;
 import org.openftc.easyopencv.OpenCvCamera;
 import org.openftc.easyopencv.OpenCvCameraFactory;
 import org.openftc.easyopencv.OpenCvCameraRotation;
-import org.openftc.easyopencv.OpenCvPipeline;
 import org.firstinspires.ftc.clockworks.algorithm.opencvpipeline.PipelineExample;
 
 @TeleOp
