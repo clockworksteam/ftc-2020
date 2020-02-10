b0691c8a8c5205f21aa0239b3a605dc7064d86c6
lucaci32u4 <lucaci32u4@gmail.com>
2020-02-10 04:39:26 +0200

    Added an option to have the robot stop after a trace

--- TeamCode/src/main/java/org/firstinspires/ftc/clockworks/algorithm/motion/Trace.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/clockworks/algorithm/motion/Trace.java
@@ -4,11 +4,13 @@ public class Trace {
     private final Point[] points;
     private final double roughTolerance;
     private final double fineTolerance;
+    private final boolean fullStop;
 
-    public Trace(Point[] points, double roughTolerance, double fineTolerance) {
+    public Trace(Point[] points, double roughTolerance, double fineTolerance, boolean fullStop) {
         this.points = points;
         this.roughTolerance = roughTolerance;
         this.fineTolerance = fineTolerance;
+        this.fullStop = fullStop;
     }
 
     public Point[] getPoints() {
@@ -21,5 +23,16 @@ public class Trace {
 
     public double getFineTolerance() {
         return fineTolerance;
+    }
+
+    public boolean hasFullstop() {
+        return fullStop;
+    }
+
+    public boolean hasPoint(Point p) {
+        for (Point pt : points) {
+            if ( p == pt) return true;
+        }
+        return false;
     }
 }
