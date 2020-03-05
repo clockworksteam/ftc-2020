172b6890fa982cd2e4aabac2edd0634f2195993a
mondern <radu.preda06@yahoo.com>
2020-03-05 12:22:29 +0200

    Rewrote teh Backservos class for the new mechanism and implementing the fiber interface

--- TeamCode/src/main/java/org/firstinspires/ftc/clockworks/hardware/BackServos.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/clockworks/hardware/BackServos.java
@@ -3,13 +3,21 @@ package org.firstinspires.ftc.clockworks.hardware;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.qualcomm.robotcore.hardware.Servo;
 
-public class BackServos {
+import org.firstinspires.ftc.clockworks.scheduler.Fiber;
+import org.firstinspires.ftc.clockworks.scheduler.InternalScheduler;
+
+public class BackServos implements Fiber {
     private Servo s;
     private Servo d;
     private boolean servosFree = true;
     private boolean l;
 
-    public void init(HardwareMap hardwareMap, String basename, boolean left) {
+    private int sp = 0;
+    private int sl = 0;
+    private int dp = 0;
+    private int dl = 0;
+
+    public void initData(HardwareMap hardwareMap, String basename, boolean left) {
         s = hardwareMap.get(Servo.class, basename + "_left");
         d = hardwareMap.get(Servo.class, basename + "_right");
 
@@ -22,16 +30,30 @@ public class BackServos {
 
     }
 
-    public void prins() {
-        if (servosFree) {
-            s.setPosition(0.4);//0.85
-            d.setPosition(0.6);
-            servosFree = false;
-        } else {
-                s.setPosition(0.95);
-                d.setPosition(0.99);
-            servosFree = true;
-        }
+
+    @Override
+    public void init(InternalScheduler scheduler) {
+        s.setDirection(Servo.Direction.FORWARD);
+        d.setDirection(Servo.Direction.REVERSE);
+
     }
 
+    @Override
+    public void tick() {
+        if (servosFree) {
+            s.setPosition(sp);//0.85
+            d.setPosition(dp);
+            servosFree = false;
+        } else {
+            s.setPosition(sl);
+            d.setPosition(dl);
+            servosFree = true;
+        }
+
+    }
+
+    @Override
+    public void deinit() {
+
+    }
 }
