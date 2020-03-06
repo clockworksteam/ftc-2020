a2bcf40c6325273a95c1d2f5dbbdb89cd3615f5b
tudorcioc3510 <tudorcioc@gmail.com>
2020-03-06 19:46:09 +0200

    Comments for the VuStoneLocalizer

--- TeamCode/src/main/java/org/firstinspires/ftc/clockworks/vision/VuStoneLocalizer.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/clockworks/vision/VuStoneLocalizer.java
@@ -34,6 +34,16 @@ public class VuStoneLocalizer {
     private boolean visible;
     private double position;
 
+    /**
+     * Here we declare the attributes of the class.
+     * The "CAMERA_CHOICE" represents a parameter that will be passed to the Vuforia engine in order to set the desired camera for the detection.
+     * The float "stoneHeight" saves the real height of the Skystone so that Vuforia will be able to calculate the position of the Skystone in its augmented reality.
+     * The String “VUFORIA_KEY” will save the license key (obtained for free from the website: developer.vuforia.com/license-manager) that will be used by the Vuforia instance. “VuforiaLocalizer” is the variable we will use to store our instance of the Vuforia localization engine and "VuforiaTrackables" is a parameter that will store a Skytone model the developers of the Robot Controller app had worked on.
+     * "visible" and "position" are some variables that will be used lately in our detection.
+     */
+
+
+
     public void init(HardwareMap map) {
         int cameraMonitorViewId = map.appContext.getResources().getIdentifier("cameraMonitorViewId", "id", map.appContext.getPackageName());
         VuforiaLocalizer.Parameters parameters = new VuforiaLocalizer.Parameters(cameraMonitorViewId);
@@ -43,12 +53,26 @@ public class VuStoneLocalizer {
 
         vuforia = ClassFactory.getInstance().createVuforia(parameters);
 
+        /**
+         * This piece of code initializes the Vuforia localization engine, by creating a new object and setting its desired properties (parameters) (like the license key and the camera name that should be used in the hardwareMap).
+         * The "cameraMonitorViewId" property makes a small rectangle on the phone's screen (called a camera view) which will show the image captured by the phone and the detection details.
+         */
+
+
+
+
         stoneTarget = vuforia.loadTrackablesFromAsset("Skystone");
         stoneTarget.get(0).setName("Skystone");
         stoneTarget.get(0).setLocation(OpenGLMatrix
                 .translation(0, 0, stoneHeight)
                 .multiplied(Orientation.getRotationMatrix(AxesReference.EXTRINSIC, AxesOrder.XYX, AngleUnit.DEGREES, 90, 0, -90)));
         stoneTarget.activate();
+
+        /**
+         *
+         * Here we set the model of the Skystone that will be used by our detection. We then tell the engine not to rotate our Skystone coordinates, as we want to get the relative y-axis coordinate for our odometry system.
+         * The last line applies the properties set for the model.
+         */
     }
 
     public void update() {
@@ -61,17 +85,46 @@ public class VuStoneLocalizer {
                 position = pos.get(0);
             } else visible = false;
         }
+
+
+        /**
+         * This method "update" is called repeatedly in our TeleOP in order to update the positions of the detected Skystones (if they exist).
+         * All the necessary calculation are made by the Vuforia augmented reality. The variable "position" will store the first Skystone detected and the boolean "visible" will be set to true.
+         * If there is no Skystone detected the boolean "visible" will be set to false.
+         *
+         *
+         *
+         *
+         *
+         *
+         */
     }
 
     public boolean isVisible() {
         return visible;
     }
 
+    /**
+     *
+     * This method "isVisible" returns the availability of a Skystone.
+     *
+     */
+
     public double getPosition() {
         return position;
     }
 
+    /**
+     * This method returns the relative y-axis coordinate of the last detected Skystone.
+     *
+     */
+
     public void stop() {
         stoneTarget.deactivate();
     }
+
+    /**
+     * This is a simple method that turns off the Vuforia augmented reality engine.
+     */
+
 }
