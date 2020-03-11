f84d8edf911f3b4bd6210a535120cd8f12fa66b7
tudorcioc3510 <tudorcioc@gmail.com>
2020-03-11 20:25:56 +0200

    Starting code for elevator

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/clockworks/hardware/elevator.java
@@ -0,0 +1,23 @@
+package org.firstinspires.ftc.clockworks.hardware;
+
+import org.firstinspires.ftc.clockworks.scheduler.Fiber
+import org.firstinspires.ftc.clockworks.scheduler.InternalScheduler;
+
+public class elevator implements Fiber  {
+
+
+    @Override
+    public void init(InternalScheduler scheduler) {
+
+    }
+
+    @Override
+    public void tick() {
+
+    }
+
+    @Override
+    public void deinit() {
+
+    }
+}
